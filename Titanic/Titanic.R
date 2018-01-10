ad796d5e25960029d2a1abda5e1793e03c7f712f
Ikjot Kaur <jot@Ikjots-MacBook-Pro.local>
2018-01-10 07:52:18 -0700

    Visualizing the data

--- Titanic/Titanic.R
+++ Titanic/Titanic.R
@@ -33,4 +33,35 @@ train.data$Age[grepl(" Dr\\.", train.data$Name) & is.na(train.data$Age)] = mean.
 train.data$Age[grepl(" Miss\\.", train.data$Name) & is.na(train.data$Age)] = mean.miss
 train.data$Age[grepl(" Master\\.", train.data$Name) & is.na(train.data$Age)] = mean.master
 
+#Exploraing and visualizing data
+barplot(table(train.data$Survived), main="Passenger Survival",  names= c("Perished", "Survived"))
+barplot(table(train.data$Pclass), main="Passenger Class",  names= c("first", "second", "third"))
+barplot(table(train.data$Sex), main="Passenger Gender")
+hist(train.data$Age, main="Passenger Age", xlab = "Age")
+barplot(table(train.data$SibSp), main="Passenger Siblings")
+barplot(table(train.data$Parch), main="Passenger Parch")
+hist(train.data$Fare, main="Passenger Fare", xlab = "Fare")
+barplot(table(train.data$Embarked), main="Port of Embarkation")
 
+counts <- table( train.data$Survived, train.data$Sex)
+barplot(counts,  col=c("darkblue","red"), legend = c("Perished", "Survived"), main = "Passenger Survival by Sex")
+counts <- table( train.data$Survived, train.data$Pclass)
+barplot(counts,  col=c("darkblue","red"), legend =c("Perished", "Survived"), main= "Titanic Class Bar Plot" )
+counts <- table( train.data$Sex, train.data$Pclass)
+barplot(counts,  col=c("darkblue","red"), legend = rownames(counts), main= "Passenger Gender by Class")
+hist(train.data$Age[which(train.data$Survived == "0")], main= "Passenger Age Histogram", xlab="Age", ylab="Count", col ="blue", breaks=seq(0,80,by=2))
+hist(train.data$Age[which(train.data$Survived == "1")], col ="red", add = T, breaks=seq(0,80,by=2))
+boxplot(train.data$Age ~ train.data$Survived, 
+        main="Passenger Survival by Age",
+        xlab="Survived", ylab="Age")
+rain.child = train.data$Survived[train.data$Age < 13]
+length(train.child[which(train.child == 1)] ) / length(train.child)
+train.youth = train.data$Survived[train.data$Age >= 15 & train.data$Age < 25]
+length(train.youth[which(train.youth == 1)] ) / length(train.youth)
+train.adult  = train.data$Survived[train.data$Age >= 20 & train.data$Age < 65]
+length(train.adult[which(train.adult == 1)] ) / length(train.adult)
+train.senior  = train.data$Survived[train.data$Age >= 65]
+length(train.senior[which(train.senior == 1)] ) / length(train.senior)
+
+mosaicplot(train.data$Pclass ~ train.data$Survived,  main="Passenger Survival Class", color=TRUE,  
+           xlab="Pclass", ylab="Survived")
