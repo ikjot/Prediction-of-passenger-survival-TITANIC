e9be13c63c56db75836a54369d539dcd53c64f6c
Ikjot Kaur <jot@Ikjots-MacBook-Pro.local>
2018-01-09 21:38:31 -0700

    Checked the percentage of missing values

--- Titanic/Titanic.R
+++ Titanic/Titanic.R
@@ -1,3 +1,9 @@
 train.data = read.csv("train.csv",na.strings = c("NA",""))
 train.data$Survived = factor(train.data$Survived)
 train.data$Pclass = factor(train.data$Pclass)
+
+# To Check the percentage of missing values
+#install.packages("Amelia")
+require(Amelia)
+missmap(train.data, main = "Missing Map")
+
